5e580950fd8a9ffb19228cfcb259858f9beadb04
kusstar <kussssss@outlook.com>
2024-07-02 21:23:57 +0800

    refactor(example): rewrite styles

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,15 +9,18 @@ import {
   LayoutAnimation,
   UIManager,
   Platform,
+  StatusBar,
 } from 'react-native';
 import { MeshGradient } from 'react-native-mesh-gradient';
 import { getColors } from 'react-native-image-colors';
 import Slider, { type SliderProps } from '@react-native-community/slider';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
     UIManager.setLayoutAnimationEnabledExperimental(true);
   }
+  SystemNavigationBar.setNavigationColor('translucent');
 }
 
 const imgs = [
@@ -39,7 +42,7 @@ const DEFAULTS = {
 const MySlider = (props: SliderProps) => {
   return (
     <Slider
-      style={{ width: 200, height: 40 }}
+      style={styles.slider}
       minimumValue={0}
       maximumValue={100}
       minimumTrackTintColor={'#FFF'}
@@ -88,6 +91,7 @@ export default function App() {
   }, [imgIndex]);
   return (
     <View style={styles.container}>
+      <StatusBar translucent backgroundColor="transparent" />
       {colors?.length === 4 && (
         <MeshGradient
           brightness={brightness}
@@ -96,31 +100,13 @@ export default function App() {
           colors={colors}
           frequency={frequency}
           amplitude={amplitude}
-          style={{
-            flex: 1,
-            width: '100%',
-            height: '100%',
-            pointerEvents: 'none',
-            position: 'absolute',
-          }}
+          style={styles.meshGradient}
         />
       )}
       {showTools && (
-        <View
-          style={{
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <View
-            style={{
-              marginTop: 16,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <View style={{ flexDirection: 'row', marginBottom: 16 }}>
+        <View style={styles.toolsContainer}>
+          <View style={styles.imgContainer}>
+            <View style={styles.imgList}>
               {imgs?.map((img, index) => (
                 <TouchableOpacity
                   key={index}
@@ -131,40 +117,26 @@ export default function App() {
                 >
                   <Image
                     source={img}
-                    style={{
-                      width: 40,
-                      height: 40,
-                      borderRadius: 8,
-                      borderWidth: index === imgIndex ? 2 : 0,
-                      borderColor: '#fff',
-                      margin: 8,
-                      opacity: index === imgIndex ? 1 : 0.5,
-                    }}
+                    style={[
+                      styles.imgToPick,
+                      // eslint-disable-next-line react-native/no-inline-styles
+                      {
+                        borderWidth: index === imgIndex ? 2 : 0,
+                        opacity: index === imgIndex ? 1 : 0.5,
+                      },
+                    ]}
                   />
                 </TouchableOpacity>
               ))}
             </View>
             <Image
               source={imgs[imgIndex]}
-              style={{
-                width: 256,
-                height: 256,
-                borderRadius: 8,
-                borderWidth: 2,
-                borderColor: '#fff',
-              }}
+              style={styles.bigImg}
               onError={(e) => {
                 console.log('error', e.nativeEvent);
               }}
             />
-            <View
-              style={{
-                flexDirection: 'row',
-                marginTop: 16,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
+            <View style={styles.colorHints}>
               {colors?.map((color, index) => (
                 <Fragment key={index}>
                   {index === 2 && (
@@ -172,115 +144,91 @@ export default function App() {
                       onPress={() => {
                         setColors(shuffle(colors));
                       }}
-                      style={{
-                        margin: 8,
-                      }}
+                      style={styles.randomBtn}
                     >
-                      <Text
-                        style={[
-                          styles.text,
-                          {
-                            fontSize: 24,
-                          },
-                        ]}
-                      >
-                        🔀
-                      </Text>
+                      <Text style={[styles.text, styles.random]}>🔀</Text>
                     </TouchableOpacity>
                   )}
-                  <View style={{ alignItems: 'center' }}>
-                    <Text
-                      style={[
-                        styles.text,
-                        {
-                          fontSize: 12,
-                        },
-                      ]}
-                    >
+                  <View style={styles.colorHintContainer}>
+                    <Text style={[styles.text, styles.colorHintLabel]}>
                       {color}
                     </Text>
                     <View
-                      style={{
-                        width: 42,
-                        height: 42,
-                        backgroundColor: color,
-                        borderRadius: 8,
-                        borderWidth: 2,
-                        borderColor: '#fff',
-                        margin: 8,
-                      }}
+                      style={[
+                        styles.colorHint,
+                        {
+                          backgroundColor: color,
+                        },
+                      ]}
                     />
                   </View>
                 </Fragment>
               ))}
             </View>
           </View>
-          <View style={{ alignItems: 'center' }}>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={[styles.text, { width: 70 }]}>Speed</Text>
+          <View style={styles.sliderSettings}>
+            <View style={styles.sliderContainer}>
+              <Text style={[styles.text, styles.sliderLabel]}>Speed</Text>
               <MySlider
                 value={speed}
                 minimumValue={0}
                 maximumValue={100}
                 onValueChange={setSpeed}
               />
-              <Text style={[styles.text, { width: 32, textAlign: 'center' }]}>
+              <Text style={[styles.text, styles.sliderValueText]}>
                 {speed.toFixed(1)}
               </Text>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={[styles.text, { width: 70 }]}>Brightness</Text>
+            <View style={styles.sliderContainer}>
+              <Text style={[styles.text, styles.sliderLabel]}>Brightness</Text>
               <MySlider
                 value={brightness}
                 minimumValue={0}
                 maximumValue={2}
                 onValueChange={setBrightness}
               />
-              <Text style={[styles.text, { width: 32, textAlign: 'center' }]}>
+              <Text style={[styles.text, styles.sliderValueText]}>
                 {brightness.toFixed(1)}
               </Text>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={[styles.text, { width: 70 }]}>Contrast</Text>
+            <View style={styles.sliderContainer}>
+              <Text style={[styles.text, styles.sliderLabel]}>Contrast</Text>
               <MySlider
-                style={{ width: 200, height: 40 }}
                 value={contrast}
                 minimumValue={0}
                 maximumValue={2}
                 onValueChange={setContrast}
               />
-              <Text style={[styles.text, { width: 32, textAlign: 'center' }]}>
+              <Text style={[styles.text, styles.sliderValueText]}>
                 {contrast.toFixed(1)}
               </Text>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={[styles.text, { width: 70 }]}>Frequency</Text>
+            <View style={styles.sliderContainer}>
+              <Text style={[styles.text, styles.sliderLabel]}>Frequency</Text>
               <MySlider
                 value={frequency}
                 minimumValue={0}
                 maximumValue={100}
                 onValueChange={setFrequency}
               />
-              <Text style={[styles.text, { width: 32, textAlign: 'center' }]}>
+              <Text style={[styles.text, styles.sliderValueText]}>
                 {frequency.toFixed(1)}
               </Text>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={[styles.text, { width: 70 }]}>Amplitude</Text>
+            <View style={styles.sliderContainer}>
+              <Text style={[styles.text, styles.sliderLabel]}>Amplitude</Text>
               <MySlider
                 value={amplitude}
                 minimumValue={0}
                 maximumValue={100}
                 onValueChange={setAmplitude}
               />
-              <Text style={[styles.text, { width: 32, textAlign: 'center' }]}>
+              <Text style={[styles.text, styles.sliderValueText]}>
                 {amplitude.toFixed(1)}
               </Text>
             </View>
             <TouchableOpacity
-              style={{
-                marginTop: 16,
-              }}
+              style={styles.resetBtn}
               onPress={() => {
                 setSpeed(DEFAULTS.speed);
                 setBrightness(DEFAULTS.brightness);
@@ -289,37 +237,19 @@ export default function App() {
                 setAmplitude(DEFAULTS.amplitude);
               }}
             >
-              <Text
-                style={[
-                  styles.text,
-                  {
-                    fontSize: 24,
-                    fontWeight: 'bold',
-                  },
-                ]}
-              >
-                ♻️
-              </Text>
+              <Text style={[styles.text, styles.reset]}>♻️</Text>
             </TouchableOpacity>
           </View>
         </View>
       )}
       <TouchableOpacity
-        style={{ position: 'absolute', bottom: 32 }}
+        style={styles.toogleToolBtn}
         onPress={() => {
           LayoutAnimation.easeInEaseOut();
           setShowTools(!showTools);
         }}
       >
-        <Text
-          style={[
-            styles.text,
-            {
-              fontSize: 32,
-              fontWeight: 'bold',
-            },
-          ]}
-        >
+        <Text style={[styles.text, styles.toggleToolText]}>
           {showTools ? '🙈' : '👀'}
         </Text>
       </TouchableOpacity>
@@ -328,12 +258,111 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  bigImg: {
+    borderColor: '#fff',
+    borderRadius: 8,
+    borderWidth: 2,
+    height: 256,
+    width: 256,
+  },
+  colorHint: {
+    borderColor: '#fff',
+    borderRadius: 8,
+    borderWidth: 2,
+    height: 42,
+    margin: 8,
+    width: 42,
+  },
+  colorHintContainer: {
+    alignItems: 'center',
+  },
+  colorHintLabel: {
+    fontSize: 12,
+  },
+  colorHints: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
   container: {
+    alignItems: 'center',
     flex: 1,
+    justifyContent: 'center',
+  },
+  imgContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 16,
+  },
+  imgList: {
+    flexDirection: 'row',
+    marginBottom: 16,
+  },
+  imgToPick: {
+    borderColor: '#fff',
+    borderRadius: 8,
+    height: 40,
+    margin: 8,
+    width: 40,
+  },
+  meshGradient: {
+    flex: 1,
+    height: '100%',
+    pointerEvents: 'none',
+    position: 'absolute',
+    width: '100%',
+  },
+  random: {
+    fontSize: 24,
+  },
+  randomBtn: {
+    margin: 8,
+    marginTop: 24,
+  },
+  reset: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  resetBtn: {
+    marginTop: 16,
+  },
+  slider: {
+    height: 40,
+    width: 200,
+  },
+  sliderContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  sliderLabel: {
+    fontSize: 16,
+    width: 80,
+  },
+  sliderSettings: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  sliderValueText: {
+    fontSize: 16,
+    textAlign: 'center',
+    width: 40,
   },
   text: {
     color: '#fff',
+  },
+  toggleToolText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  toogleToolBtn: {
+    bottom: 24,
+    position: 'absolute',
+  },
+  toolsContainer: {
+    marginTop: -64,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 });
